b58dfff860a887705bf80a46cf41e305b4386e01
Marco Oliveri <marcooliveri94@gmail.com>
2023-05-01 18:34:09 +0200

    Add files via upload

---
+++ motion.js
@@ -0,0 +1,88 @@
+export const transition = { type: "spring", duration: 0.8 };
+
+export const buttonBump = {
+  whileHover: { scale: 1.1 },
+  whileTap: { scale: 0.9 },
+  transition: { type: "spring", stiffness: 400, damping: 17 },
+};
+
+export const fadeTopAnimation = {
+  initial: { y: 10, opacity: 0},
+  animate: { y: 0, opacity: 1 },
+  exit: { y: -10, opacity: 0 },
+  transition: { duration: 0.2 }
+};
+export const fadeBottomAnimation = {
+  initial: { y: -10, opacity: 0},
+  animate: { y: 0, opacity: 1 },
+  exit: { y: -10, opacity: 0 },
+  transition: { duration: 0.4 }
+};
+export const slideAnimation = (direction) => {
+  return {
+    initial: {
+      x: direction === "left" ? -100 : direction === "right" ? 100 : 0,
+      y: direction === "up" ? 100 : direction === "down" ? -100 : 0,
+      opacity: 0,
+      transition: { ...transition, delay: 0.5 },
+    },
+    animate: {
+      x: 0,
+      y: 0,
+      opacity: 1,
+      transition: { ...transition, delay: 0 },
+    },
+    exit: {
+      x: direction === "left" ? "-100%" : direction === "right" ? "100%" : 0,
+      y: direction === "up" ? 100 : direction === "down" ? -100 : 0,
+      transition: { ...transition, delay: 0 },
+    },
+  };
+};
+
+export const fadeAnimation = {
+  initial: {
+    opacity: 0,
+    transition: { ...transition, delay: 0.5 },
+  },
+  animate: {
+    opacity: 1,
+    transition: { ...transition, delay: 0 },
+  },
+  exit: {
+    opacity: 0,
+    transition: { ...transition, delay: 0 },
+  },
+};
+
+export const headTextAnimation = {
+  initial: { x: 100, opacity: 0 },
+  animate: { x: 0, opacity: 1 },
+  transition: {
+    type: "spring",
+    damping: 5,
+    stiffness: 40,
+    restDelta: 0.001,
+    duration: 0.3,
+  },
+};
+
+export const headContentAnimation = {
+  initial: { y: 100, opacity: 0 },
+  animate: { y: 0, opacity: 1 },
+  transition: {
+    type: "spring",
+    damping: 7,
+    stiffness: 30,
+    restDelta: 0.001,
+    duration: 0.6,
+    delay: 0.2,
+    delayChildren: 0.2,
+  },
+};
+
+export const headContainerAnimation = {
+  initial: { x: -100, opacity: 0, transition: { ...transition, delay: 0.5 } },
+  animate: { x: 0, opacity: 1, transition: { ...transition, delay: 0 } },
+  exit: { x: -100, opacity: 0, transition: { ...transition, delay: 0 } },
+};
